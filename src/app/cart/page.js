038f6b7704b3ab02770579d5d2b43f3a8fdf4d1d
laxmi789 <93903213+laxmi789@users.noreply.github.com>
2026-01-18 14:36:25 +0530

    Update page.js

--- src/app/cart/page.js
+++ src/app/cart/page.js
@@ -89,8 +89,7 @@ export default function Cart() {
   return (
     <>
       <Header />
-    {Array.isArray(cart) && cart.length > 0 ? (
-  
+    {cart && cart.length > 0 ? (  
     <div className="max-w-5xl max-lg:max-w-2xl mx-auto p-4">
           <h1 className="text-xl font-semibold text-slate-900">Shopping Cart</h1>
           <div className="w-full py-10">
